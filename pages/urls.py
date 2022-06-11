67d58b0faadc4f263cf1a3a5510812e4c6a7955e
Rkpano05 <rk.pani2002@gmail.com>
2022-06-11 11:17:37 +0530

    First Django commit

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('',home, name='home'),
+    path('about/',about_page,name='about'),
+    path('services/',services_page,name='services'),
+    path('contact/',contact_page,name='contact'),
+]
