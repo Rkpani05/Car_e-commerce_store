9170dcecd5178d9d9639f65865e0cb58feebec46
Rkpano05 <rk.pani2002@gmail.com>
2022-06-15 12:51:46 +0530

    file commited

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Team
+from django.utils.html import format_html
 
 # Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self,object):
+        return format_html('<img src="{}" width="20" style="border-radius:50px;"/>'.format(object.photo.url))
+    list_display= ['id','thumbnail','first_name','designation','created_date'] 
+    
+    
+admin.site.register(Team,TeamAdmin)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,18 @@
+
 from django.db import models
 
+
 # Create your models here.
+
+class Team(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    designation = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='image/%y/%m/%d')
+    facebook_link = models.URLField(max_length=500)
+    twitter_link = models.URLField(max_length=500)
+    google_plus_link = models.URLField(max_length=500)
+    created_date = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self) :
+        return self.first_name
